c13ee80de5675c0d2989265a9f9819962042cbcc
teaforthecat <teaforthecat@gmail.com>
2013-02-16 22:06:56 -0600

    fix css require

--- walker/app_builder.rb
+++ walker/app_builder.rb
@@ -66,8 +66,10 @@ private
 
   def fake_file
     template 'files/fake_file.js.coffee', 'lib/assets/javascripts/fake_file.js.coffee'
+    template 'files/fake_file.css.scss', 'lib/assets/stylesheets/fake_file.css.scss'
     template 'files/fake_file_input.rb',  'app/inputs/fake_file_input.rb'
     append_file 'app/assets/javascripts/application.js',  "//= require fake_file\n"
+    append_file 'app/assets/stylesheets/application.css.scss',  "/*= require fake_file */\n"
   end
 
   def root_controller name
